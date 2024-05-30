741002bfcf6703214503fb47f60b623a31f35527
cjs <cjswntjd@gmail.com>
2024-05-30 13:01:05 +0900

    테스트를 위한 webView sample code 추가

--- app/src/main/java/com/example/floating_logger/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/floating_logger/ui/dashboard/DashboardFragment.kt
@@ -1,9 +1,14 @@
 package com.example.floating_logger.ui.dashboard
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.webkit.JavascriptInterface
+import android.webkit.WebChromeClient
+import android.webkit.WebViewClient
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -32,11 +37,44 @@ class DashboardFragment : Fragment() {
         dashboardViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
+
+        textView.setOnClickListener {
+
+        }
+
+        initWebView()
+
         return root
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    private fun initWebView() {
+        with(binding.webView) {
+            settings.javaScriptEnabled = true
+            webViewClient = WebViewClient()
+            webChromeClient = WebChromeClient()
+
+            // interface 추가
+            addJavascriptInterface(WebAppInterface(), "Android")
+
+            // html 콘텐츠 로드
+            loadUrl("file:///android_asset/sample.html")
+        }
+        /**
+         * webView Log 형태
+         * chromium                com.example.floating_logger          I  [INFO:CONSOLE(9)] "Hello from WebView!", source: file:///android_asset/sample.html (9)
+         */
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private class WebAppInterface {
+        @JavascriptInterface
+        fun logMessage(message: String) {
+            Log.d("WebView", "[WEBVIEW] $message")
+        }
     }
 }
